0efafb93464f3d6115957c1007770bfa813d8955
[urugai27] <[uehara_atusi@nifty.com]>
2021-09-02 07:42:24 +0900

    20210902  bat 誤り訂正

--- make/insIndex.bat
+++ make/insIndex.bat
@@ -4,7 +4,7 @@ set htmldir=%makedir:\make=%
 set dirHomepage=D:\homepage
 if "%COMPUTERNAME%" == "DESKTOP-7QG32IQ"  set dirHomepage=C:\Users\ATHUSHI\homepage
 
-copy %dirHomepage%\index.html	%htmldir%\old_index.html
+copy %dirHomepage%\index.html	%htmldir%\make\old_index.html
 
 copy %htmldir%\tmp_edit.html temp_index.html
 
