c09434ec4a81a04e52686a9674e373d0c32d1ea6
[urugai27] <[uehara_atusi@nifty.com]>
2022-05-31 16:52:17 +0900

    20220531 mayaa today giima のindex を一括で作成する

---
+++ allindex.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem	"https://urugai04.bitbucket.io"
+echo カミマヤーを起動します	
+call mayaa.bat
+echo カミマヤーのバッチファイルから戻りました
+
+
+rem	"https://tanagaa.bitbucket.io/"
+echo うるがいある日を起動します
+call today.bat
+echo うるがいある日のバッチファイルから戻りました
+
+
+rem	"https://urugai.web.fc2.com/"
+echo タナガーを起動します
+call tanagaa.bat
+echo タナガーのバッチファイルから戻りました
+
+
+rem	"https://urugai27.web.app"
+echo ギーマを起動します
+call giima.bat
+echo ギーマのバッチファイルから戻りました
